1a3ec2f5c7b65f1acd83382585277490c4846e37
iarikaja <iarykaja@gmail.com>
2025-02-16 20:44:44 +0300

    fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,10 @@
-// import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    // <Router basename="/">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    // {/* </Router> */}
+     <h1 className="text-3xl font-bold underline">
+     Hello world!
+   </h1>
   );
 }
 
