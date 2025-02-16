bf5834966e25f9094b86994cdc4c2c6f41e794e2
iarikaja <iarykaja@gmail.com>
2025-02-16 20:15:13 +0300

    rectif

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <Router basename="/portfolio">
+    // <Router basename="/">
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-    </Router>
+    // {/* </Router> */}
   );
 }
 
