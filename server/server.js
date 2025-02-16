f15e93037a4e86700978bbdbe7017204b53b06ac
iarikaja <iarykaja@gmail.com>
2025-02-16 22:37:09 +0300

    cors

--- server/server.js
+++ server/server.js
@@ -6,22 +6,29 @@ const PORT = process.env.PORT || 5050;
 const app = express();
 
 const allowedOrigins = [
-  'https://iarikaja.github.io/porfolio',  // Modifie avec ton URL frontend
+  'https://iarikaja.github.io',  // Modifie avec ton URL frontend
   'http://localhost:3000',  // Si tu utilises localhost pour le développement
 ];
 
 
-app.use(cors
-  ({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('CORS not allowed'), false);
-      }
-    }
-  })
-);
+// app.use(cors
+//   ({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('CORS not allowed'), false);
+//       }
+//     }
+//   })
+// );
+
+app.use(cors({
+  origin: 'https://iarikaja.github.io',  // Autorisez uniquement votre domaine frontend
+  methods: ['GET', 'POST'],  // Vous pouvez ajuster les méthodes HTTP autorisées
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
 app.use(express.json());
 app.use("/record", records);
 
