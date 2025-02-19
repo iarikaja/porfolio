45572c8557fa8b9c02ff8e664a5d9fb91ee22dcb
iarikaja <iarykaja@gmail.com>
2025-02-19 07:19:45 +0300

    db fix

--- server/db/connection.js
+++ server/db/connection.js
@@ -13,17 +13,31 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
-try {
-  // Connect the client to the server
-  await client.connect();
-  // Send a ping to confirm a successful connection
-  await client.db("admin").command({ ping: 1 });
-  console.log(
-   "Pinged your deployment. You successfully connected to MongoDB!"
-  );
-} catch(err) {
-  console.error(err);
+// try {
+//   // Connect the client to the server
+//   await client.connect();
+//   // Send a ping to confirm a successful connection
+//   await client.db("admin").command({ ping: 1 });
+//   console.log(
+//    "Pinged your deployment. You successfully connected to MongoDB!"
+//   );
+// } catch(err) {
+//   console.error(err);
+// }
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
+run().catch(console.dir);
 
 let db = client.db(dataBase);
 
